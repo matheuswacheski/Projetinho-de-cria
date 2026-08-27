51d775bd62489d2e4db2a2dc0577077ce6790b93
Matheus Wacheski <125933774+matheuswacheski@users.noreply.github.com>
2026-08-27 15:37:45 -0300

    Melhora alarme e configura clock de 4 MHz

--- motor.h
+++ motor.h
@@ -2,8 +2,9 @@
 #define MOTOR_H
 
 #include <xc.h>
+#include <stdint.h>
 
-#define _XTAL_FREQ 20000000
+#define _XTAL_FREQ 4000000
 
 
 // Inicializa o controle do motor
@@ -16,6 +17,9 @@ void motor_start(void);
 
 // Desliga o motor
 void motor_stop(void);
+
+// Fecha a porta pelo tempo máximo configurado e desliga o motor.
+void motor_close_for_time(void);
 
 
 #endif
